0048f36a8fa41e4513d43d535b321bf1577fccdf
hcbarros <hcb@a.recife.ifpe.edu.br>
2022-03-29 00:05:05 -0300

    commit

--- src/test/java/br/com/bankaccountapi/BankAccountApiApplicationTests.java
+++ src/test/java/br/com/bankaccountapi/BankAccountApiApplicationTests.java
@@ -6,9 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import br.com.bankaccountapi.enums.Flag;
+import br.com.bankaccountapi.enums.TypeCard;
 import br.com.bankaccountapi.models.Account;
+import br.com.bankaccountapi.models.Card;
+import br.com.bankaccountapi.models.Type;
 import br.com.bankaccountapi.services.auth.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -20,6 +25,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.junit.jupiter.api.Order;
 import org.springframework.http.*;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -37,14 +43,22 @@ class BankAccountApiApplicationTests {
     @Autowired
     private AuthService authService;
 
-    private HttpEntity<LoginRequest> httpEntity;
+    private HttpHeaders headers = new HttpHeaders();
+
+    @BeforeEach
+    public void setUp() {
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        JwtResponse jwt = authService.authenticateUser("henrique", "henrique123");
+        headers.setBearerAuth(jwt.getToken());
+    }
 
 
     @Order(1)
     @Test
     public void whenTrySigninWithInvalidcredentials_thenBadCredentialsShouldBeReturned() {
 
-        setHttpEntity("henrique","henrique12");
+        HttpEntity<LoginRequest> httpEntity = new HttpEntity<LoginRequest>(
+                new LoginRequest("henrique","henrique12"), headers);
 
         ResponseEntity<String> response = restTemplate
                 .postForEntity("http://localhost:" + port + "/api/v1/auth/signin/", httpEntity, String.class);
@@ -57,7 +71,8 @@ class BankAccountApiApplicationTests {
     @Test
     public void whenTrySigninWithValidcredentials_thenStatusOKShouldBeReturned() {
 
-        setHttpEntity("henrique","henrique123");
+        HttpEntity httpEntity = new HttpEntity<LoginRequest>(new LoginRequest(
+                "henrique", "henrique123"), headers);
 
         ResponseEntity<JwtResponse> response = restTemplate
                 .postForEntity("http://localhost:" + port + "/api/v1/auth/signin/", httpEntity, JwtResponse.class);
@@ -75,7 +90,7 @@ class BankAccountApiApplicationTests {
         Set<String> roles = new HashSet<>();
         roles.add("USER");
         HttpEntity<SignupRequest> httpEntity = new HttpEntity<SignupRequest>
-                (new SignupRequest("usuario_teste","teste123", roles), new HttpHeaders());
+                (new SignupRequest("usuario_teste","teste123", roles), headers);
 
         ResponseEntity<String> response = restTemplate
                 .postForEntity("http://localhost:" + port + "/api/v1/auth/signup/", httpEntity, String.class);
@@ -88,7 +103,8 @@ class BankAccountApiApplicationTests {
     @Test
     public void whenSearchAccountById_thenAccountShouldBeReturned() {
 
-        setHttpEntity("henrique","henrique123");
+        HttpEntity httpEntity = new HttpEntity<LoginRequest>(new LoginRequest(
+                "henrique", "henrique123"), headers);
 
         ResponseEntity<Account> response = restTemplate.exchange(
                 "http://localhost:" + port + "/api/v1/accounts/1/", HttpMethod.GET, httpEntity, Account.class);
@@ -100,22 +116,35 @@ class BankAccountApiApplicationTests {
     @Test
     public void whenTrySaveAccount_thenAccountShouldBeSaveAndReturned() {
 
-        setHttpEntity("henrique","henrique123");
+        HttpEntity<Account> httpAccount = new HttpEntity<Account>
+                (new Account("Name test", "1234",
+                        "12345678", "0", null), headers);
 
-        ResponseEntity<Account> response = restTemplate.exchange(
-                "http://localhost:" + port + "/api/v1/accounts/1/", HttpMethod.GET, httpEntity, Account.class);
+        ResponseEntity<Account> response = restTemplate
+                .postForEntity("http://localhost:" + port + "/api/v1/accounts/", httpAccount, Account.class);
 
-        assertEquals(response.getBody().getNameOwner(), "Henrique Barros");
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        assertNotNull(response.getBody().getId());
+        assertEquals(response.getBody().getNameOwner(), "Name test");
     }
 
+    @Order(6)
+    @Test
+    public void shouldUpdateAccount() {
 
+        HttpEntity<Account> httpAccount = new HttpEntity<Account>
+                (new Account("Name test changed", "4567",
+                        "87654321", "1", null), headers);
 
-    private void setHttpEntity(String username, String password) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        JwtResponse jwt = authService.authenticateUser("henrique", "henrique123");
-        headers.setBearerAuth(jwt.getToken());
-        httpEntity = new HttpEntity<LoginRequest>(new LoginRequest(username, password), headers);
+        restTemplate.put("http://localhost:" + port + "/api/v1/accounts/edit/2", httpAccount, "id");
+
+        ResponseEntity<Account> response = restTemplate.exchange(
+                "http://localhost:" + port + "/api/v1/accounts/2", HttpMethod.GET, httpAccount, Account.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody().getId(), 2);
+        assertEquals(response.getBody().getNameOwner(), "Name test changed");
+        assertEquals(response.getBody().getAccountCode(), "87654321");
     }
 
 }
